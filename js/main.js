86ada20077b5d4d30d21a13622da1c60efbc6a7b
Pavel Dumitrescu <89450504+PavelDumitrescu@users.noreply.github.com>
2022-02-14 16:34:13 +0100

    contact (almost done)

--- js/main.js
+++ js/main.js
@@ -39,7 +39,15 @@ window.onclick = function(event) {
   }
 }
 
-
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
 
 
 
