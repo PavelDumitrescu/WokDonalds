71d6af5f5c7b199c1608750c44c85b3fa3dce1e9
LeonBeLeoff <leon.bagger.torang@student.tabyenskilda.se>
2022-03-02 12:14:39 +0100

    Javascript adjustments.

--- js/second.js
+++ js/second.js
@@ -38,3 +38,13 @@ window.onclick = function(event) {
     }
   }
 }
+
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
